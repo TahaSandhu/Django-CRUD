fbd5658f36d6316257c9d38bd17773ec6e85f81e
TahaSandhu <tahab9044@gmail.com>
2023-12-26 13:30:16 -0800

    CRUD APP in Django

--- Home/Home/urls.py
+++ Home/Home/urls.py
@@ -20,4 +20,12 @@ from CrudOperation import views
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', views.welcome),
+     path('loadform',views.load_form),
+     path('add',views.add),
+     path('showData',views.showData),
+     path('edit/<int:id>',views.edit),
+     path('update/<int:id>',views.update),
+     path('delete/<int:id>',views.delete),
+     path('search',views.search),
+
 ]
